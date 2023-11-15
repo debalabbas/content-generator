fcdc3b50078b76ed7514528ac1d414c24b00ad32
Debal Abbas <debalabbas7@gmail.com>
2023-11-16 05:15:37 +0530

    Final Changes

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # content-generator
 To develop a LLM that mimics Justin Welsh, and produces content by taking input from the user
+
+## Data Collection
+I have extracted 9 articles from his website on Personal Growth, Business Planning and Social Media Growth.
+After that I have created a training dataset by splitting all of them into chunks of 265 words, with 100 overlapping words, to have a better context while training.
+
+## Model Training
+
+I have used `gpt-2` and trained the model using Masked Language Modelling(Next TOekn Prediction), for 150 epochs.
+
+## Inference
+I tried some prompting techniques, and found out that the one that primes the latent space of the model works best, have also added the comparrison in the notebook of the results, following is the final prompt:
+```
+Main Question: <What is the sub question for that category> ?
+Dialog 1: Well, first I need to think about <thinking about the category in general> in general.
+Dialog 2: Next, maybe I need to figure out how I define the answer. What criteria am I looking to judge on?
+Dialog 3: Based on all this, what can I answer ?
+```
+
+## Interface
+I have developed a streamlit app, that accepts the main category and then asks for a detailed question within that category. Which is then used to generate the answer
--- app.py
+++ app.py
@@ -2,28 +2,43 @@ import streamlit as st
 from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 # Initialize the model and tokenizer
-tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
-model = GPT2LMHeadModel.from_pretrained("gpt2")
+tokenizer = GPT2Tokenizer.from_pretrained("debal/gpt2-finetuned-justin-welsh")
+model = GPT2LMHeadModel.from_pretrained("debal/gpt2-finetuned-justin-welsh")
 
 # Define your prompt template
-prompt_template = "The person said: {}, and they also said: {}"
+prompt_template = """Main Question: {} ?
+Dialog 1: Well, first I need to think about {} in general.
+Dialog 2: Next, maybe I need to figure out how I define the answer. What criteria am I looking to judge on?
+Dialog 3: Based on all this, what can I answer ?
+Answer:"""
 
 def ask_questions():
     # Ask the user a couple of questions
-    answer1 = st.text_input('What can I help you with today, ')
-    answer2 = st.text_input('Question 2')
+    category = st.radio(
+        "Select a category",
+        ("Personal Growth", "Business Planning", "Social Media Growth")
+    )
+    answer = st.text_input(f'What can I help you in {category}')
+    print(bool(answer))
 
     # Feed the answers to the prompt template
-    prompt = prompt_template.format(answer1, answer2)
-
-    return prompt
+    prompt = prompt_template.format(answer,category)
+    print(prompt)
+    return prompt,answer
 
 def generate_response(prompt):
     # Encode the prompt
     inputs = tokenizer.encode(prompt, return_tensors="pt")
 
     # Generate a response
-    outputs = model.generate(inputs, max_length=150, num_return_sequences=1)
+    outputs = model.generate(
+        inputs, 
+        max_length=300, 
+        num_return_sequences=1, 
+        num_beams=3, 
+        no_repeat_ngram_size=3, 
+        top_k=30, 
+        top_p=1)
 
     # Decode the response
     response = tokenizer.decode(outputs[0], skip_special_tokens=True)
@@ -31,11 +46,13 @@ def generate_response(prompt):
     return response
 
 def main():
-    prompt = ask_questions()
-    if st.button('Generate Response'):
+    st.title('Justin Welsh Bot')
+    prompt,sub_question = ask_questions()
+    if bool(sub_question):
         with st.spinner('Generating...'):
             response = generate_response(prompt)
-            st.write(response)
+            # response = response.replace(prompt,"")
+            st.write(response[len(prompt)-1:])
 
 if __name__ == "__main__":
     main()
