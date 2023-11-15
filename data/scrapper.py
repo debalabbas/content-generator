d1165d2c30ba9134e24c2bea579216902bfa3df1
Debal Abbas <debalabbas7@gmail.com>
2023-11-16 00:14:14 +0530

    Updated training data

--- data/scrapper.py
+++ data/scrapper.py
@@ -5,22 +5,22 @@ files = os.listdir('datasets/')
 
 input = []
 for f in files:
+    if '.txt' in f:
+        with open(f'datasets/{f}', 'r') as f:
 
-    with open(f'datasets/{f}', 'r') as f:
-
-        text = f.read()
-    text = text.replace('\n','')
-    words = text.split(' ')
-    
-    for i in range(0,len(words) + 100,128):
-
-        if i == 0:
-            temp = words[i:(i+1)*128]
-        else:
-            temp = words[i-100:(i+1)*128 - 100]
+            text = f.read()
+        text = text.replace('\n','')
+        words = text.split(' ')
         
-        input.append(' '.join(temp))
-    
+        for i in range(0,len(words) + 100,256):
+
+            if i == 0:
+                temp = words[i:i+256]
+            else:
+                temp = words[i-100:i+256 - 100]
+            
+            input.append(' '.join(temp))
+
 
 with open('datasets/train.json','w') as f:
 
